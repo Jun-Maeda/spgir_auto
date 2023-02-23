d235fed73f33960cfff886e38463f7b5d828a4f0
maedajun <jun.mermaid.voice@gmail.com>
2023-02-23 22:35:59 +0900

    Fix:2を管理者権限で実行

--- sh/2action.sh
+++ sh/2action.sh
@@ -1,3 +1,3 @@
 cd /home/spgir_auto
 source venv/bin/activate
-echo 2 | nohup python sc.py > main_logs/$(date "+%Y_%m_%d_%H_%M_%S").out &
+sudo echo 2 | nohup python sc.py > main_logs/$(date "+%Y_%m_%d_%H_%M_%S").out &
