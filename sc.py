2f5ef3e7d78d45f0d41b4a28708e81e19de2ec0a
maedajun <jun.mermaid.voice@gmail.com>
2023-06-26 22:59:46 +0900

    Fix:ファイルのパーミッション変更

--- sc.py
+++ sc.py
@@ -279,7 +279,7 @@ class Spgirl_Auto:
                             driver.get(f"https://spgirl.cityheaven.net/J10ComeonAiMatchingList.php?gid={self.username}")
                             WebDriverWait(driver, 10).until(
                                 EC.visibility_of_element_located((By.CLASS_NAME, "kitene_mada")))
-                        time.sleep(5)
+                        time.sleep(30)
                         btns = driver.find_elements(By.CLASS_NAME, value='kitene_mada')
                         time.sleep(5)
                         print(btns)
